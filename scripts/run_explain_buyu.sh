6120c00e71addf943d90921270db08de197c1244
boychaboy <hoon2j@gmail.com>
2021-08-23 12:44:23 +0900

    fix script fixed

--- scripts/run_explain_buyu.sh
+++ scripts/run_explain_buyu.sh
@@ -10,11 +10,11 @@ GPU_ID=$1
 while(( $buyu < $max_iter ))
 do
     echo "Current 补语 is ${buyu}"
-    python main.py --data_dir ./data/buyu/--bert_model bert-base-chinese \
-        --max_seq_length 128 --output_dir runs/buyu_${buyu} lm_dir runs/lm_buyu \
-        --task_name buyu --eval_batch_size 4 \
+    python main.py --data_dir ./data/buyu/ --bert_model bert-base-chinese \
+        --max_seq_length 128 --output_dir runs/buyu_${buyu} \
+        --lm_dir runs/lm_buyu --task_name buyu --eval_batch_size 1 \
         --hiex --hiex_add_itself --output_filename hiex.pkl \
         --nb_range 5 --sample_n 3 --algo soc --do_eval --test --explain \
-        --gpu_id $GPU_ID --only_positive --buyu ${buyu} \
+        --gpu_id $GPU_ID --only_positive --buyu ${buyu}
     let buyu++
 done
